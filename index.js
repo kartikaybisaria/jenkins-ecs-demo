340e4a8ccf8ddbbbb9a317d9cd686b62c546ec75
Ktk <-add>
2025-07-31 11:49:48 +0530

    push it

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins ECS Agent!');
+  res.send('Hello from Jenkins ECS Agent xxx!');
 });
 
 app.listen(port, () => {
